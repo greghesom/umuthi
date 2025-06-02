e0e463c46ff681359b64721ad0c1f526b30cb5c1
Greg <greg@hesom.net>
2025-06-02 13:07:54 +0200

    function running

--- umuthi.AppHost/Program.cs
+++ umuthi.AppHost/Program.cs
@@ -1,10 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.umuthi_ApiService>("apiservice");
+var functionApp = builder.AddProject<Projects.umuthi_Functions>("functions");
 
 builder.AddProject<Projects.umuthi_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService)
+    .WithReference(functionApp)
     .WaitFor(apiService);
 
 builder.Build().Run();
--- umuthi.Functions/Program.cs
+++ umuthi.Functions/Program.cs
@@ -1,14 +1,17 @@
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Builder;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
 
-var builder = FunctionsApplication.CreateBuilder(args);
+var builder = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
 
-builder.ConfigureFunctionsWebApplication();
+        // Add your custom services here
+        // services.AddScoped<IMyService, MyService>();
+    });
 
-builder.Services
-    .AddApplicationInsightsTelemetryWorkerService()
-    .ConfigureFunctionsApplicationInsights();
-
-builder.Build().Run();
+var host = builder.Build();
+await host.RunAsync();
